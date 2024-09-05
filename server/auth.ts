86c4b71ed1bf1fd9a367f42aee6df58be302b679
“gave” <adeun2020@gmail.com>
2024-09-05 12:58:09 -0700

    6

--- server/auth.ts
+++ server/auth.ts
@@ -8,6 +8,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
      session: {
           strategy: "jwt"
      },
+     secret: process.env.AUTH_SECRET,
+     debug: true, // enable debug logs
      providers: [],
      callbacks: {
           jwt({ token, user }) {
