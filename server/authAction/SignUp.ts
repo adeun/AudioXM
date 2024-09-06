5ff0ff9288ff93b4147b65a745ff584dee7f3b40
“gave” <adeun2020@gmail.com>
2024-09-06 03:54:23 -0700

    77865

--- server/authAction/SignUp.ts
+++ server/authAction/SignUp.ts
@@ -3,9 +3,28 @@
 import { zodSignUpForm } from "@/lib/ZOD";
 import prisma from "../db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 
 export default async function SignUpRoute(data:z.infer< typeof zodSignUpForm>) {
      const {email ,password ,name ,birthDay ,PhoneNumber} =data
+     try {
+          const hashedPassword = await bcrypt.hash(password, 10)
+          const user = await prisma.user.create({
+               data: {
+                    email,
+                    password: hashedPassword,
+                    name,
+                    birthDay,
+                    PhoneNumber
+               },
+          })
+          
+          
+          
+     } catch (error) {
+          console.log(error);
+          
+     }
      
 }
