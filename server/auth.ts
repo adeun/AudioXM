386fcbb8d08f51e644c81c5c8379f77b9f7f3560
“gave” <adeun2020@gmail.com>
2024-09-06 03:11:15 -0700

    656

--- server/auth.ts
+++ server/auth.ts
@@ -1,6 +1,19 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import Credentials from "next-auth/providers/credentials"
 import prisma from "./db"
+import bcrypt from "bcrypt";
+import { zodLoginForm } from "@/lib/ZOD"
+type Login = {
+     createdAt: Date;
+     updatedAt: Date;
+     id: string;
+     email: string;
+     emailVerified: Date | null;
+     name: string | null;
+     image: string | null;
+     password: string;
+} | null
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -10,12 +23,53 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
      },
      secret: process.env.AUTH_SECRET,
      debug: true, // enable debug logs
-     providers: [],
+     providers: [
+          Credentials({
+               // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+               // e.g. domain, username, password, 2FA token, etc.
+               credentials: {
+                    email: {},
+                    password: {},
+               },
+               authorize: async (credentials) => {
+                    try {
+
+
+                         const { email, password } = await zodLoginForm.parseAsync(Credential)
+
+                         // find the user
+                         const getUser = await prisma.user.findFirst({
+                              where: {
+                                   email: email,
+                              },
+                         })
+                         // if the user is not found
+                         if (!getUser) { return null; }
+
+                         // validate the password
+                         const validate = await bcrypt.compare(getUser.password, password)
+                         if (validate) {
+                              return getUser;
+                         }
+
+
+
+
+                         // return JSON object with the user data
+                         return null
+                    } catch (error) {
+
+                         return null
+                    }
+               },
+          }),
+
+     ],
      callbacks: {
           jwt({ token, user }) {
                if (user && user.id) { // User is available during sign-in
                     token.id = user.id
-                    
+
                }
                return token
           },
@@ -27,15 +81,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                     , user: {
                          ...session.user,
                          id: token.id,
-                         
+
                     }
                }
           },
           signIn(params) {
                return true
           },
-
-
 
 
      },
