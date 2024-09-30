7e7f4d3c851666febe3310e62676782412bd1c2f
“gave” <adeun2020@gmail.com>
2024-09-30 04:06:20 -0700

    4.2C-1.1

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,22 +7,15 @@ export default async function NotFound() {
      const headersList = headers()
      const domain = headersList.get('host')
      console.log(`Domain: ${domain}`)
-     
+
      return (
           <div className=' flex flex-1 items-center justify-center'>
-               <h2 className=' text-3xl'>
-                    <ShieldAlert className=' text-red-600' />
+               <h2 className=' text-3xl'> <ShieldAlert className=' text-red-600' /> Album Not Found <ShieldAlert className=' text-red-600' /></h2>
 
-                     Not Found 
-
-                    <ShieldAlert className=' text-red-600' />
-               </h2>
                <p className=' text-lg'>Could not find requested resource</p>
 
                <Link className=' text-xl' href="/Home">
-                    <Button>
-                         <House />   Return Home
-                    </Button>
+                    <Button> <House /> Return Home</Button>
                </Link>
 
 
