358df33938d4ad2a8974b5a589eb9005c4fd2575
“gave” <adeun2020@gmail.com>
2024-09-30 04:02:37 -0700

    4.2C-1

--- app/Album/[albumId]/page.tsx
+++ app/Album/[albumId]/page.tsx
@@ -3,7 +3,10 @@ import PlayListPage from '@/components/page/playListPage'
 import { auth } from '@/server/auth'
 import FindAlbum from '@/server/songAction/FindAlbum'
 import React from 'react'
-
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { House } from 'lucide-react';
+import { ShieldAlert } from 'lucide-react';
 export default async function page({ params }: { params: { albumId: string } }) {
   const getAlbum = await FindAlbum({ albumId: params.albumId })
   const Session = await auth()
@@ -12,8 +15,21 @@ export default async function page({ params }: { params: { albumId: string } }) 
     return (
       <>
         <main className=' flex flex-col gap-2 flex-1 items-center justify-center'>
-          <h1>Album Not Found</h1>
-          <p>The album you are looking for does not exist.</p>
+          
+          <h2 className=' text-3xl'>
+            <ShieldAlert className=' text-red-600' />
+
+            Album Not Found
+
+            <ShieldAlert className=' text-red-600' />
+          </h2>
+          <p className=' text-lg'>The album you are looking for does not exist.</p>
+
+          <Link className=' text-xl' href="/Home">
+            <Button>
+              <House />   Return Home
+            </Button>
+          </Link>
         </main>
       </>
     )  // Redirect to 404 page or error page if album not found.
@@ -22,9 +38,9 @@ export default async function page({ params }: { params: { albumId: string } }) 
 
   return (
     <>
-    <Nav2 Search={null} user={Session}/>
+      <Nav2 Search={null} user={Session} />
       <main className=' flex flex-col gap-2 flex-1'>
-        <PlayListPage album={getAlbum}/>
+        <PlayListPage album={getAlbum} />
 
 
       </main>
